db4b79adc53bd95a3d48be25d17a87c695f858e3
DylanFarkas <dylan.farkas@correounivalle.edu.co>
2025-02-19 15:21:19 -0500

    actualization3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/srv/staticfiles'
 
 # Configuración de archivos de medios (imágenes)
 MEDIA_URL = '/media/'
